bf892e6851686f91ce3c5999148568f7f24054d3
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2026-03-31 22:49:00 +0100

    feat: learn operators in python

--- hello.py
+++ hello.py
@@ -2,3 +2,18 @@ greeting = "hello world"
 print(greeting)
 
 # To start the python terminal, run the command "python" in your command line interface.
+
+# You can run divisions, addition, subtraction, and multiplication in the python terminal below
+
+# >>> meaning = 42
+# >>> meaning += 1
+# >>> meaning
+
+# >>> meaning *= 10
+# >>> meaning 
+# 430 
+# >>> meaning /= 12.3
+# >>> meaning 
+# 34.959349593495936
+# >>> meaning = round(meaning)
+# >>> meaning
